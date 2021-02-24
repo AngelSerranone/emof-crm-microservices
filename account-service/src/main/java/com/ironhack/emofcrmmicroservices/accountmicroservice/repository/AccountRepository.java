c5e283ec1819689b4926665db7d7e274ac4bd801
Adrián Maza <adri_mv1@hotmail.com>
2021-02-24 21:06:40 +0100

    query fix

--- account-service/src/main/java/com/ironhack/emofcrmmicroservices/accountmicroservice/repository/AccountRepository.java
+++ account-service/src/main/java/com/ironhack/emofcrmmicroservices/accountmicroservice/repository/AccountRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
-	@Query("SELECT CAST(a.country AS int), CAST(COUNT(*) AS int) FROM OpportunityId o JOIN Account a ON o.account = a.id GROUP BY o.account")
+	@Query("SELECT a.country, CAST(COUNT(*) AS int) FROM OpportunityId o JOIN Account a ON o.account = a.id GROUP BY o.account")
 	List<Object[]> getOppsByCountry();
 }
